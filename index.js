1d97a69562b094e60c95c3c73b9ea379881ac85a
Rafa <rafacruiz@gmail.com>
2025-10-15 17:17:11 +0200

    lab javascript algorithms Bonus 1

--- index.js
+++ index.js
@@ -63,31 +63,59 @@ function charactersReverse (hackerName) {
         newLetter += hacker;
     }
 
-    console.log(newLetter);
+    console.info(newLetter);
 }
 
 function lexicoGraphic (hacker1, hacker2) {
 
-    if (hacker1 === hacker2) {
-        console.log("What?! You both have the same name?");
-        return;
-    }
+    const name1 = hacker1.toLowerCase();
+    const name2 = hacker2.toLowerCase();
 
-    for (let i = 0; i < hacker1.length; i++) {
-        const driver = hacker1[i];
-        const navegator = hacker2[i];
+    for (let i = 0; i < Math.min(name1.length, name2.length); i++) {
+        const driver = name1[i];
+        const navegator = name2[i];
         
         if (driver < navegator) {
-            console.log("The driver's name goes first");
+            console.info("The driver's name goes first.");
             return;
-        } else {
-            console.log("Yo, the navigator goes first, definitely.");
+        } else if (driver > navegator) {
+            console.info("Yo, the navigator goes first, definitely.");
             return;
         }
     }
+
+    if (name1.length < name2.length) {
+        console.info("The driver's name goes first.");
+        return;
+    } else if (name1.length > name2.length) {
+        console.info("Yo, the navigator goes first, definitely.");
+        return;
+    } else {
+        console.info("What?! You both have the same name?");
+    }
 }
 
-const hacker1 = prompt("Enter the driver's name");
-const hacker2 = prompt("Enter the navegator`s name");
+const name1 = prompt("Enter the driver's name");
+const name2 = prompt("Enter the navegator`s name");
 
-inputNames(hacker1, hacker2);
+inputNames(name1, name2);
+
+function generateParagraphs () {
+    const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mi massa, blandit at porta eu, convallis eu sapien. Cras eget fermentum erat. Donec vulputate pretium tortor vel pretium. Nam dui diam, bibendum id mi at, feugiat placerat velit. Curabitur dapibus urna a magna laoreet egestas. Ut dapibus justo quis malesuada ornare. Pellentesque tincidunt vehicula consectetur. Cras facilisis, ante in pretium sagittis, diam velit suscipit eros, cursus rutrum nunc dolor et lorem. Cras sed tempus lorem. Curabitur et interdum lectus. Nam eu turpis ultrices, dignissim dui et, malesuada nisi. Phasellus ut fringilla nibh. Phasellus bibendum sapien at magna lacinia, eget lacinia eros feugiat. Sed fermentum diam diam, sit amet placerat dolor sagittis eu. Nam ultricies malesuada risus, a vestibulum lacus vestibulum sit amet. Fusce non nulla ut nisi gravida efficitur sit amet porttitor metus. Sed tempus enim vel viverra congue. Aliquam vestibulum, tortor ut cursus maximus, felis felis posuere leo, id facilisis mi urna non massa. Phasellus et suscipit tortor. Morbi tortor enim, venenatis in tincidunt et, lacinia quis odio. Praesent ornare pretium vestibulum. Donec blandit pharetra semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque dapibus vulputate fringilla. Phasellus elit felis, fringilla nec tellus non, laoreet dapibus turpis. Nullam consequat enim sed tristique mattis. Donec lorem quam, volutpat a arcu eget, mollis interdum risus. Nulla facilisi. Praesent a bibendum arcu, ut consectetur magna. Nunc tincidunt ante ac lacus accumsan accumsan. Suspendisse potenti. Vivamus est massa, auctor mollis facilisis in, posuere congue enim. In accumsan, purus sit amet condimentum pulvinar, turpis turpis ultrices turpis, in dictum massa nunc in sem. Duis non interdum mauris. Quisque efficitur ipsum vel odio porta, faucibus posuere quam tempor. In a iaculis dui. Fusce pharetra sapien nibh.";
+    const paragraphs = longText.split(' ');
+    const countWords = paragraphs.length;
+    let countTimes = 0;
+
+    for (let i = 0; i < countWords; i++) {
+        const paragraph = paragraphs[i];
+
+        if (paragraph.includes('et')) {
+            countTimes++;
+        }        
+    }
+    
+    console.log('Número de palabras: ', countWords);
+    console.log("Número de veces 'et': ", countTimes);
+}
+
+generateParagraphs();
